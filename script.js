ea8a1df2380ff103f1a503898f5921820e4e059e
malikfazlehadi-555 <malikfazlehadi03333@gmail.com>
2024-09-07 03:23:51 -1200

    text

---
+++ script.js
@@ -0,0 +1,24 @@
+// TASK-1
+//ALERT
+// let name = prompt("enter you name")
+// alert("welcome to SMIT" + " " +name)
+//..................................................
+
+// //TASK-2
+//ADD Number
+// let firstnumber = +prompt("enter the firstnumber")
+// let secoundnumber = +prompt("enter the secoundnumber")
+// alert(firstnumber + secoundnumber)
+//..................................................................
+
+//TASK-3
+//LEGAL VARIABLE
+// let name = ("sufyan")
+// let NAME1 = ("sufyan")
+// let -name = ("sufyan")
+// let $name =("sufyan")
+
+// //IILEGAL VARIABLE
+// let 1 name =("sufyan")
+// let name- =("sufyan")
+// let my name = ("sufyan")
