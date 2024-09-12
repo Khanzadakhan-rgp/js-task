b7940146e599af5372cc52ba224e250877f141be
malikfazlehadi-555 <malikfazlehadi03333@gmail.com>
2024-09-12 03:52:49 -1200

    text

--- script.js
+++ script.js
@@ -22,3 +22,82 @@
 // let 1 name =("sufyan")
 // let name- =("sufyan")
 // let my name = ("sufyan")
+//...............................................................
+
+// //TASK-4
+//CALCULATOR
+// let operator = prompt("enter operator(either +,- *,/)")
+// let num1 = parseFloat(prompt("enter first number"))
+// let num2 = parseFloat(prompt("enter second number"))
+
+// if (operator == "+") {alert(num1 + num2)} 
+// else if (operator == "-") {alert(num1 - num2)} 
+// else if (operator == "*") {alert(num1 * num2)} 
+// else {alert(num1 /num2)};
+// console.log("${num1} ${operator} ${num2} = ${alert}")
+//.....................................................................
+
+// //TASK-5
+// let no1 = prompt("enter no1")
+// let no2 = prompt("enter n02")
+// let modulus = no1 % no2
+// alert("the remainder of " + " "+ no1+ " "+ "divided by" +" "+ no2+ " "+ "is" + " " + modulus)
+//.....................................................................................................
+
+// //TASK-6
+// let num1 = 9
+// let num2 = 12
+// num1++
+// num2--
+// alert(num1)
+// alert(num2)
+//..............................................................
+
+//TASK-7
+// let xyz = ( 30+30 -20 / 40*2)
+// alert(xyz)
+//........................................
+
+//TASK-8
+// let first = ("hello")
+// let second = ("muhammad")
+// let third = ("sufyan")
+// alert(first + second + third)
+//........................................
+
+//TASK-9
+
+// let Name = prompt("what is you Name ")
+// let age = prompt("enter you age")
+// alert("Welcome " + " "+Name+ " "+ "you are "+ " "+ age + " "+ "years old")
+//.........................................................................................
+
+//TASK-10
+// let Number = prompt("enter 1st number")
+// if (Number == '0'){alert("zero")}
+// else if (Number < 0){alert("negative")}
+// else if (Number > 0){alert("positive")}
+// else(alert("wrong"))
+//.....................................................
+
+//TEST-11
+// let firstnumber = prompt("enter 1st number")
+// let secondnumber = prompt("enter 2nd number")
+// if (firstnumber > secondnumber){alert("1st number is greater then secondnumber")}
+// else if (secondnumber < firstnumber){alert("1st number is less then secondnumber")}
+// else (alert("equal Number"))
+//............................................................................................
+
+//TESK-12
+// let score = prompt("enter you score")
+// if (score>=90){alert("A")}
+// else if (score>=80){alert("A")}
+// else if (score>=70){alert("B+")}
+// else if (score>=60){alert("B")}
+// else if (score>=50){alert("poor")}
+// else (alert("fail"))
+//........................................................................
+
+//TASK-13
+let fruits = ["apple" , "banana" , "oringe" , "guava" , "mango"]
+for (let i = 0 ; i < 5 ; i++){alert("fruits")}
